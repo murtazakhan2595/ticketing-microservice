2f8c5b2097c1d8e07d7e23f1a57f7a716f5f6e02
Ahmed Murtaza <murtazakhan2595@gmail.com>
2026-03-13 15:54:45 +0500

    Connecting to mongodb

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import 'express-async-errors'
 import {json} from 'body-parser';
+import mongoose from 'mongoose';
 import { currentUserRouter } from './routes/current-user';
 import { signinRouter } from './routes/signin';
 import { signoutRouter } from './routes/signout';
@@ -10,7 +11,6 @@ import { NotFoundError } from './errors/not-found-error';
 
 const app = express();
 app.use(json())
-
 
 app.use(currentUserRouter);
 app.use(signinRouter);
@@ -23,6 +23,15 @@ app.all("*",async()=>{
 
 app.use(errorHandler)
 
-app.listen(3000,()=>{
-  console.log('Listening on port 3000!!')
-})
+const start = async () => {
+ try{
+   await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
+  app.listen(3000,()=>{
+    console.log('Listening on port 3000!!')
+  })
+ }catch(err){
+  console.error(err)
+ }
+}
+
+start();
